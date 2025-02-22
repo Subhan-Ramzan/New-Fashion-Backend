e16188ce251148ec4ce959acbfa7508c9c878025
Subhan <subhanramzan215@gmail.com>
2025-02-22 13:16:02 +0500

    JWT

--- config/plugins.js
+++ config/plugins.js
@@ -1,36 +1,38 @@
-const fs = require("fs");
-const path = require("path");
-const heicConvert = require("heic-convert");
-
 module.exports = ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET'), // Read JWT Secret from .env file
+    },
+  },
   upload: {
     config: {
-      provider: "@strapi/provider-upload-cloudinary",
+      provider: '@strapi/provider-upload-cloudinary',
       providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
       actionOptions: {
         upload: async (file) => {
-          // Check if the uploaded file is in HEIC format
-          if (file.mime === "image/heic" || file.mime === "image/heif") {
+          const fs = require('fs');
+          const path = require('path');
+          const heicConvert = require('heic-convert');
+
+          if (file.mime === 'image/heic' || file.mime === 'image/heif') {
             const inputBuffer = fs.readFileSync(file.tmpPath);
             const outputBuffer = await heicConvert({
               buffer: inputBuffer,
-              format: "JPEG",
-              quality: 0.8, // Adjust quality as needed
+              format: 'JPEG',
+              quality: 0.8,
             });
 
-            // Define new file path and name
-            const newFileName = `${file.name.split(".")[0]}.jpg`;
-            const newFilePath = path.join("/tmp", newFileName);
+            const newFileName = `${file.name.split('.')[0]}.jpg`;
+            const newFilePath = path.join('/tmp', newFileName);
             fs.writeFileSync(newFilePath, Buffer.from(outputBuffer));
 
-            // Update file properties for Cloudinary upload
             file.path = newFilePath;
             file.name = newFileName;
-            file.mime = "image/jpeg";
+            file.mime = 'image/jpeg';
           }
 
           return file;
