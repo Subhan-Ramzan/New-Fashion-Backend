fc1279af45adc280ab07164bbd55d9d5e7393865
Subhan <subhanramzan215@gmail.com>
2025-02-22 14:29:53 +0500

    Jwt

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,14 @@
+
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'default-admin-secret'), // Default value if missing
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'default-api-token-salt'), // Default value if missing
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'default-transfer-token-salt'), // Default value if missing
     },
   },
   flags: {
--- config/plugins.js
+++ config/plugins.js
@@ -1,3 +1,4 @@
+//config/plugin.js
 module.exports = ({ env }) => ({
   "users-permissions": {
     config: {
