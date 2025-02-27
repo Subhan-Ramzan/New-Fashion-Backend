cce8ef5ba0b97eae080c7eb867afecc46624dce0
Subhan <subhanramzan215@gmail.com>
2025-02-28 00:33:14 +0500

    Order_Change

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', process.env.PORT || 1337), // Ensure it binds to Render's port
   app: {
-    keys: env('APP_KEYS', 'defaultKey1,defaultKey2').split(','), // Fix for APP_KEYS issue
+    keys: env('APP_KEYS', 'defaultKey1,defaultKey2').split(','),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
